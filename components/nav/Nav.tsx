fe042df2d216d7cf76b7f8d1001310d8fae1a266
FrancooChaparro <francoo_chaparro@hotmail.com>
2024-04-24 10:59:53 -0300

    modal-nav

--- components/nav/Nav.tsx
+++ components/nav/Nav.tsx
@@ -1,14 +1,41 @@
-import React from 'react';
+"use client"
+import React, { useState } from "react";
 import styles from "./Nav.module.css";
 
 export const Nav = () => {
+  const [showCor, setShowCor] = useState(false);
+
+  const handleMouseEnterCor = () => {
+    setShowCor(true);
+  };
+
+  const handleMouseLeaveCor = () => {
+    setShowCor(false);
+  };
   return (
-    <div className={styles["container-all"]}>
-        <div><span>LEPRECHAUN</span></div>
-        <div className={styles["container-data"]}>
-            <span>CATEGORIAS</span><span>CONTACTO</span><span>CONOCENOS</span>
-        </div>
-        <div><span>CARRITO</span></div>
+    <div className={styles["container-all"]}> 
+      <div>
+        <span>LEPRECHAUN</span>
+      </div>
+      <div className={styles["container-data"]}>
+        <span  onMouseEnter={handleMouseEnterCor}>CATEGORIAS</span>
+        <div className={showCor ? styles.cor : styles.co}>
+            {showCor && (
+              <div
+                className={styles.cor}
+                onMouseEnter={handleMouseEnterCor}
+                onMouseLeave={handleMouseLeaveCor}
+              >
+               
+              </div>
+            )}
+          </div>
+        <span  onMouseEnter={handleMouseLeaveCor}>CONTACTO</span>
+        <span  onMouseEnter={handleMouseLeaveCor}>CONOCENOS</span>
+      </div>
+      <div>
+        <span>CARRITO</span>
+      </div>
     </div>
-  )
-}
+  );
+};

029f57e0a4b83c852c900c675f5f3bb1c3a78aae
FrancooChaparro <francoo_chaparro@hotmail.com>
2024-04-24 17:03:45 -0300

    footer-app

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
 import { Banner } from "@/components/banner/Banner";
 import styles from "./page.module.css";
+import { Footer } from "@/components/footer/Footer";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Banner />
+      <div style={{height: "100vh"}}></div>
+      <div style={{display: "flex", justifyContent: "center"}}>
+      <hr style={{width: "80%"}} />
+
+      </div>
+      <Footer />
     </main>
   );
 }
---
+++ components/footer/Footer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./footer.module.css";
+
+export const Footer = () => {
+  return (
+    <div className={styles["container-footer"]}>
+      © 2023 LEPRECHAUN destailing shop - Todos los derechos reservados
+    </div>
+  );
+};

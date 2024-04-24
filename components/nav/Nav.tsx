c9ea4234c245ea87b02d1b777cfad2dff75ae552
FrancooChaparro <francoo_chaparro@hotmail.com>
2024-04-24 16:05:43 -0300

    just-banner

--- components/nav/Nav.tsx
+++ components/nav/Nav.tsx
@@ -1,9 +1,12 @@
-"use client"
+"use client";
 import React, { useState } from "react";
 import styles from "./Nav.module.css";
+import { categorias } from "@/models/products";
+import { Product } from "@/types/types";
 
 export const Nav = () => {
   const [showCor, setShowCor] = useState(false);
+  const products: Product[] = categorias;
 
   const handleMouseEnterCor = () => {
     setShowCor(true);
@@ -13,28 +16,46 @@ export const Nav = () => {
     setShowCor(false);
   };
   return (
-    <div className={styles["container-all"]}> 
+    <div className={styles["container-all"]}>
       <div>
-        <span>LEPRECHAUN</span>
+        <span className={styles["btn-nav"]}>LEPRECHAUN</span>
       </div>
       <div className={styles["container-data"]}>
-        <span  onMouseEnter={handleMouseEnterCor}>CATEGORIAS</span>
+      <span className={styles["btn-nav"]} onMouseEnter={handleMouseLeaveCor}>
+          CONTACTO
+        </span>
+        <span className={styles["btn-nav"]} onMouseEnter={handleMouseEnterCor}>
+          CATEGORIAS
+        </span>
         <div className={showCor ? styles.cor : styles.co}>
-            {showCor && (
-              <div
-                className={styles.cor}
-                onMouseEnter={handleMouseEnterCor}
-                onMouseLeave={handleMouseLeaveCor}
-              >
-               
-              </div>
-            )}
-          </div>
-        <span  onMouseEnter={handleMouseLeaveCor}>CONTACTO</span>
-        <span  onMouseEnter={handleMouseLeaveCor}>CONOCENOS</span>
+          {showCor && (
+            <div
+              className={styles.cor}
+              onMouseEnter={handleMouseEnterCor}
+              onMouseLeave={handleMouseLeaveCor}
+            >
+              <div className={styles["center"]}>
+              {products.length &&
+                products.map((e) => {
+                  return (
+                     <div className={styles["container-categorias"]}>
+                       <span className={styles["categorias"]} key={e.id}>
+                         {e.nombre}
+                       </span>
+                     </div>
+                  );
+                })} 
+                </div>
+             </div>
+          )}
+        </div>
+       
+        <span className={styles["btn-nav"]} onMouseEnter={handleMouseLeaveCor}>
+          CONOCENOS
+        </span>
       </div>
       <div>
-        <span>CARRITO</span>
+        <span className={styles["btn-nav"]}>CARRITO</span>
       </div>
     </div>
   );

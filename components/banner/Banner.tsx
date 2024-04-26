8487f3c1e55dee95b9f9d81053856aaf2126ba33
FrancooChaparro <francoo_chaparro@hotmail.com>
2024-04-26 15:59:38 -0300

    contact-component

--- components/banner/Banner.tsx
+++ components/banner/Banner.tsx
@@ -1,19 +1,18 @@
 import Image from 'next/image'
 import React from 'react'
 import styles from "./banner.module.css"
-import poster from "@/public/images/ajax_poster.webp";
 
 export const Banner = () => {
   return (
-    <div className={styles.ContainerBackground}>
-      {/* <Image
-        src={poster}
+    <div className={styles["container-background"]}>
+       {/* <Image
+        src="https://radiotemuco.com/home/wp-content/uploads/2022/07/Foto1.jpg"
         alt="poster"
         fill
         loading="lazy"
         placeholder="blur" 
         blurDataURL="/images/ajax_poster_blur.webp"
-      /> */}
+      />  */}
       <img src="https://radiotemuco.com/home/wp-content/uploads/2022/07/Foto1.jpg" alt="alt" />
       <div className={styles["container-btn"]}>
         <button className={styles["btn-banner"]}>VER PRODUCTOS</button>

720e341afe81ff7eb24afc87387b0d1428565f05
FrancooChaparro <francoo_chaparro@hotmail.com>
2024-04-29 16:12:32 -0300

    shoop-status

--- app/(routes)/shoop/page.tsx
+++ app/(routes)/shoop/page.tsx
@@ -1,21 +1,36 @@
-import React from 'react';
+import React from "react";
 import styles from "./shoop.module.css";
-import { ProductCard } from '@/components/product-card/ProductCard';
+import { ProductCard } from "@/components/product-card/ProductCard";
 
 export const Shoop = () => {
   return (
     <div className={styles["container-main-shoop"]}>
       <div className={styles["container-main-shoop-data"]}>
-          <h4>Guantes</h4>
-          <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia tenetur quibusdam sit.</h4>
+        <h2>Guantes</h2>
+        <h4>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+          tenetur quibusdam sit.
+        </h4>
       </div>
       <div className={styles["container-main-shoop-actions"]}>
-          <div>FILTRAR</div>
-          <div>ORDENAR</div>
+        <div className={styles["filter"]}><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M912 192H328c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h584c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8zm0 284H328c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h584c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8zm0 284H328c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h584c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8zM104 228a56 56 0 1 0 112 0 56 56 0 1 0-112 0zm0 284a56 56 0 1 0 112 0 56 56 0 1 0-112 0zm0 284a56 56 0 1 0 112 0 56 56 0 1 0-112 0z"></path></svg><label htmlFor="">FILTRAR</label></div>
+        <div className={styles["order"]}>
+          <label htmlFor="">ORDENAR</label>{" "}
+          <select name="" id="">
+            <option value="name" data-direction="asc">
+              Nombre A - Z{" "}
+            </option>
+            <option value="name" data-direction="asc">
+              Nombre A - Z{" "}
+            </option>
+            <option value="name" data-direction="asc">
+              Nombre A - Z{" "}
+            </option>
+          </select>
+        </div>
       </div>
-      <div  className={styles["test"]}>
-
-      <div className={styles["container-main-shoop-cards"]}>
+      <div className={styles["test"]}>
+        <div className={styles["container-main-shoop-cards"]}>
           <ProductCard />
           <ProductCard />
           <ProductCard />
@@ -30,11 +45,10 @@ export const Shoop = () => {
           <ProductCard />
           <ProductCard />
           <ProductCard />
-      </div>
+        </div>
       </div>
     </div>
-  )
-}
-
+  );
+};
 
 export default Shoop;

330792449d6a10ddb7a1487329020d87e2736c84
FrancooChaparro <francoo_chaparro@hotmail.com>
2024-04-30 11:06:43 -0300

    30

--- app/(routes)/shoop/page.tsx
+++ app/(routes)/shoop/page.tsx
@@ -2,11 +2,14 @@
 import React, { useEffect, useState } from "react";
 import styles from "./shoop.module.css";
 import { ProductCard } from "@/components/product-card/ProductCard";
+import { Filter } from "@/components/filter/Filter";
 
 export const Shoop = () => {
   const [showBackground, setShowBackground] = useState<boolean>(false);
+  const [show, setShow] = useState<boolean>(false);
   const TOP_OFFSET = 400;
 
+ 
 
   useEffect(() => {
     const handleScroll = () => {
@@ -25,6 +28,10 @@ export const Shoop = () => {
   }, []);
   return (
     <div className={styles["container-main-shoop"] }>
+    
+        <Filter isOpen={show} />
+
+    
       <div className={styles["container-main-shoop-data"]}>
         <h2>Guantes</h2>
         <h4>
@@ -37,9 +44,9 @@ export const Shoop = () => {
 
 
       <div className={styles["container-main-shoop-actions"]}>
-        <div className={styles["filter"]}><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" height="1.2em" width="1.2em" xmlns="http://www.w3.org/2000/svg"><path d="M912 192H328c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h584c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8zm0 284H328c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h584c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8zm0 284H328c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h584c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8zM104 228a56 56 0 1 0 112 0 56 56 0 1 0-112 0zm0 284a56 56 0 1 0 112 0 56 56 0 1 0-112 0zm0 284a56 56 0 1 0 112 0 56 56 0 1 0-112 0z"></path></svg><label htmlFor="">FILTRAR</label></div>
+        <div className={styles["filter"]}><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" height="1.2em" width="1.2em" xmlns="http://www.w3.org/2000/svg"><path d="M912 192H328c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h584c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8zm0 284H328c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h584c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8zm0 284H328c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h584c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8zM104 228a56 56 0 1 0 112 0 56 56 0 1 0-112 0zm0 284a56 56 0 1 0 112 0 56 56 0 1 0-112 0zm0 284a56 56 0 1 0 112 0 56 56 0 1 0-112 0z"></path></svg><label  htmlFor="">FILTRAR</label></div>
         <div className={styles["order"]}>
-          <label htmlFor="">ORDENAR</label>{" "}
+          <label htmlFor="" onClick={()=> setShow(!show)}>ORDENAR</label>{" "}
           <select name="" id="">
             <option value="name" data-direction="asc">
               Nombre A - Z{" "}
---
+++ components/filter/Filter.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from "./filter.module.css"
+
+export const Filter = ({ isOpen } : { isOpen: boolean}) => {
+  return (
+    <>
+    {isOpen && <div className={styles['overlay-menu-container']} />}
+    <section className={`${styles["container-section-burger-menu"]} ${isOpen ? styles["active"] : ""}`}>
+        <div className={styles["wraper"]}>
+           <h1>CARRITO</h1>
+        </div>
+    </section>
+</>
+  )
+}

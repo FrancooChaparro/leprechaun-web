ce3d8341a45daa4ac3cd71165b76a4507d5f42b2
FrancooChaparro <francoo_chaparro@hotmail.com>
2024-07-26 16:12:22 -0300

    btn-cart

--- components/product-card/ProductCard.tsx
+++ components/product-card/ProductCard.tsx
@@ -10,14 +10,21 @@ export const ProductCard = () => {
         <div className={styles["container-card-image"]}>
             <img src="https://t2.uc.ltmcdn.com/es/posts/6/8/6/productos_de_limpieza_caseros_para_el_coche_37686_600.webp" alt="product" />
         </div>
+
         <div className={styles["container-card-data"]}>
             <span className={`${styles["name"]} ${inter.className}`}>
-            {/* <span className={styles["name"]}> */}
                 Cepillo tapizado
+            </span>
+            <span className={`${styles["description"]} ${inter.className}`}>
+                Description of the product - leprechaun
             </span>
             <span className={styles["price"]}>
                 $3000
             </span>
+        </div>
+
+        <div className={`${styles["container-btn"]}`}>
+            <button className={`${styles["buy-button"]}`}>AGREGAR AL CARRITO</button>
         </div>
     </div>
   )

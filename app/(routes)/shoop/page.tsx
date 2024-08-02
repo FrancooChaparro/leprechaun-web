5c6196643ef2931c6cb546f9a18b456f20cb760c
FrancooChaparro <francoo_chaparro@hotmail.com>
2024-08-02 15:38:42 -0300

    cart-component

--- app/(routes)/shoop/page.tsx
+++ app/(routes)/shoop/page.tsx
@@ -31,7 +31,7 @@ export const Shoop = () => {
   return (
     <div className={styles["container-main-shoop"] }>
     
-        <Filter isOpen={show} />
+        {/* <Filter isOpen={show} /> */}
 
     
       <div className={styles["container-main-shoop-data"]}>
@@ -46,7 +46,7 @@ export const Shoop = () => {
       <div className={styles["container-main-shoop-actions"]}>
         <div className={styles["filter"]}><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" height="1.2em" width="1.2em" xmlns="http://www.w3.org/2000/svg"><path d="M912 192H328c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h584c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8zm0 284H328c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h584c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8zm0 284H328c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h584c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8zM104 228a56 56 0 1 0 112 0 56 56 0 1 0-112 0zm0 284a56 56 0 1 0 112 0 56 56 0 1 0-112 0zm0 284a56 56 0 1 0 112 0 56 56 0 1 0-112 0z"></path></svg><label  htmlFor="">FILTRAR</label></div>
         <div className={styles["order"]}>
-          <label htmlFor="" onClick={()=> setShow(!show)}>ORDENAR</label>
+          <label htmlFor="" >ORDENAR</label>
           <select name="" id="">
             <option value="name" data-direction="asc">
               Nombre A - Z
